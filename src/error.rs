3191cbc45a9c7d6155b72377bf65e002b11d2f45
Tomas Krizek <tomas.krizek@mailbox.org>
2020-12-29 01:15:01 +0100

    board: interpret notation and string init

--- src/error.rs
+++ src/error.rs
@@ -11,3 +11,14 @@ impl fmt::Display for NotationError {
         write!(f, "Invalid notation")
     }
 }
+
+#[derive(Debug)]
+pub struct TierError;
+
+impl Error for TierError { }
+
+impl fmt::Display for TierError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid tier")
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
+use std::collections::HashMap;
 use std::convert::TryFrom;
-use std::option::Option;
-use crate::error::NotationError;
+use crate::error::{NotationError, TierError};
 
 pub mod error;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Color {
     Red,
     Green,
@@ -30,7 +30,7 @@ impl TryFrom<&str> for Color {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Tier {
     First,
     Second,
@@ -56,11 +56,10 @@ impl TryFrom<&str> for Tier {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Stack {
     color: Color,
     tier: Tier,
-    // TODO position: Option<>,
 }
 
 impl TryFrom<&str> for Stack {
@@ -78,9 +77,94 @@ impl TryFrom<&str> for Stack {
     }
 }
 
+#[allow(non_camel_case_types)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum Position {
+    i1,
+    i2,
+    i3,
+    i4,
+    j1,
+    j2,
+    j3,
+    j4,
+    k1,
+    k2,
+    k3,
+    k4,
+    l1,
+    l2,
+    l3,
+    l4,
+}
+
+impl TryFrom<&str> for Position {
+    type Error = NotationError;
+
+    fn try_from(notation: &str) -> Result<Self, Self::Error> {
+        match notation {
+            "i1" => Ok(Position::i1),
+            "i2" => Ok(Position::i2),
+            "i3" => Ok(Position::i3),
+            "i4" => Ok(Position::i4),
+            "j1" => Ok(Position::j1),
+            "j2" => Ok(Position::j2),
+            "j3" => Ok(Position::j3),
+            "j4" => Ok(Position::j4),
+            "k1" => Ok(Position::k1),
+            "k2" => Ok(Position::k2),
+            "k3" => Ok(Position::k3),
+            "k4" => Ok(Position::k4),
+            "l1" => Ok(Position::l1),
+            "l2" => Ok(Position::l2),
+            "l3" => Ok(Position::l3),
+            "l4" => Ok(Position::l4),
+            _ => Err(NotationError),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Board {
-    stacks: [[Option<Stack>; 4]; 4],
+    slots: HashMap<Position, Stack>,
+}
+
+impl Board {
+    fn new() -> Board {
+        Board {
+            slots: HashMap::with_capacity(16),
+        }
+    }
+
+    fn interpret(notation: &str) -> Result<(Position, Stack), NotationError> {
+        if notation.len() != 4 {
+            return Err(NotationError);
+        }
+        let position = Position::try_from(&notation[2..])?;
+        let stack = Stack::try_from(&notation[0..2])?;
+        Ok((position, stack))
+    }
+}
+
+impl TryFrom<&str> for Board {
+    type Error = NotationError;
+
+    fn try_from(notations: &str) -> Result<Self, Self::Error> {
+        let mut board = Board::new();
+        notations.split(" ").try_fold(
+            &mut board,
+            |board, notation: &str| {
+                let (position, stack) = Board::interpret(notation)?;
+                if board.slots.contains_key(&position) {
+                    Err(NotationError)
+                } else {
+                    board.slots.insert(position, stack);
+                    Ok(board)
+                }
+            }
+        )?;
+        Ok(board)
+    }
 }
 
 #[cfg(test)]
@@ -122,6 +206,51 @@ mod tests {
         assert!(Stack::try_from("r0").is_err());
         assert!(Stack::try_from("e3").is_err());
         assert!(Stack::try_from("sdfsf").is_err());
+        assert!(Stack::try_from("x").is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn position_notation() -> Result<(), NotationError> {
+        Position::try_from("i1")?;
+        Position::try_from("i2")?;
+        Position::try_from("i3")?;
+        Position::try_from("i4")?;
+        Position::try_from("j1")?;
+        Position::try_from("j2")?;
+        Position::try_from("j3")?;
+        Position::try_from("j4")?;
+        Position::try_from("k1")?;
+        Position::try_from("k2")?;
+        Position::try_from("k3")?;
+        Position::try_from("k4")?;
+        Position::try_from("l1")?;
+        Position::try_from("l2")?;
+        Position::try_from("l3")?;
+        Position::try_from("l4")?;
+        assert!(Position::try_from("r3").is_err());
+        assert!(Position::try_from("I2").is_err());
+        assert!(Position::try_from("dafd").is_err());
+        assert!(Position::try_from("x").is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn board_interpret() -> Result<(), NotationError> {
+        Board::interpret("r3i1")?;
+        Board::interpret("y4k3")?;
+        Board::interpret("r3i1")?;
+        assert!(Board::interpret("r3i1 ").is_err());
+        assert!(Board::interpret("x").is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn board_notation() -> Result<(), NotationError> {
+        Board::try_from("r3i1")?;
+        Board::try_from("r3i1 y4k3")?;
+        assert!(Board::try_from("r3i1 r3i1").is_err());
+        assert!(Board::try_from("r3i1 y4k3 ").is_err());
         Ok(())
     }
 }
