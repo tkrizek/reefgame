e20beb11cc88f718af6c7e3ed9862607dc14c4b6
Tomas Krizek <tomas.krizek@mailbox.org>
2020-12-29 23:04:22 +0100

    pattern: add tier matching

--- src/pattern.rs
+++ src/pattern.rs
@@ -2,7 +2,7 @@ use std::vec::Vec;
 use std::collections::HashSet;
 use strum::IntoEnumIterator;
 
-use crate::{Board, Color, Position};
+use crate::{Board, Color, Position, Tier};
 
 type Mask = HashSet<Position>;
 
@@ -13,7 +13,6 @@ trait Pattern {
 impl Pattern for Color {
     fn fit(&self, board: &Board) -> Vec<Mask> {
         let mut matches = Vec::new();
-
         for position in Position::iter() {
             if let Some(stack) = board.slots.get(&position) {
                 if stack.color == *self {
@@ -21,7 +20,20 @@ impl Pattern for Color {
                 }
             }
         }
+        matches
+    }
+}
 
+impl Pattern for Tier {
+    fn fit(&self, board: &Board) -> Vec<Mask> {
+        let mut matches = Vec::new();
+        for position in Position::iter() {
+            if let Some(stack) = board.slots.get(&position) {
+                if stack.tier == *self {
+                    matches.push([position].iter().cloned().collect());
+                }
+            }
+        }
         matches
     }
 }
@@ -47,6 +59,25 @@ mod tests {
             [Position::j1].iter().cloned().collect(),
         ]);
         assert_eq!(Color::Blue.fit(&board).len(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn tier_fit() -> Result<(), Error> {
+        let board = Board::try_from("r3i2 r1i3 g2j4 y1j1 r4k1")?;
+        assert_eq!(Tier::First.fit(&board), vec![
+            [Position::i3].iter().cloned().collect(),
+            [Position::j1].iter().cloned().collect(),
+        ]);
+        assert_eq!(Tier::Second.fit(&board), vec![
+            [Position::j4].iter().cloned().collect(),
+        ]);
+        assert_eq!(Tier::Third.fit(&board), vec![
+            [Position::i2].iter().cloned().collect(),
+        ]);
+        assert_eq!(Tier::Fourth.fit(&board), vec![
+            [Position::k1].iter().cloned().collect(),
+        ]);
         Ok(())
     }
 }
